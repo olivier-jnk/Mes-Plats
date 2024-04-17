9fd0807ef0b33db9c427c33b625e0d9d7043a7ac
Olivier <151942968+olivier-jnk@users.noreply.github.com>
2024-04-17 23:37:29 +0200

    Update doc_c_submit.php
    Incrémentation du nom du dossier si autre dossier avec le meme nom à peaufinner encore, + ajout d'un fichier et ecriture dedans (pas encore fonctionnel)

--- docs_creator/doc_c_submit.php
+++ docs_creator/doc_c_submit.php
@@ -2,13 +2,20 @@
 
 session_start();
 
-// traiter les données et creer les dossiers / fichiers en fonction.
+// traiter les données / fichiers.
+// Systeme d'incrémentation si fichier avec le meme nom.
 
-// Faire le systeme d'incrémentation pour faire en sorte que le nouveau dossier ne puisse pas avoir le nom d'un autre.
-
-// $folder_name = $_POST['doc_name'];
 $folder_name = $_POST['doc_name'];
 $first_folder_path = "./documents/{$folder_name}";
+
+$i = 0;
+while (file_exists($first_folder_path)) {
+    echo "File exist !";
+    $first_folder_path = $first_folder_path . $i;
+    $i++;
+}
+// Si "jean" existe -> "jean1" creer, mais si "jean" et "jean1" existe et création de "jean" -> "jean12" créer !
+// Revoir le code
 
 // './documents/' . $folder_name
 if (!mkdir($first_folder_path)){
@@ -18,7 +25,24 @@ if (!mkdir($first_folder_path)){
 }
 
 // Créer un fichier et ecrire dedans.
-// Créer un autre dossier stockant les PJs'
+// Stocker les différentes pj dans le dossier aproprié + creer un fichier txt et marquer des choses dedans avec les infos 
+// récupérées.
+
+
+$file_name = "file.txt";
+$txt = "Test {$folder_name}";
+
+$create = fopen($file_name, 'w');
+
+if ($create === false) {
+    die("Impossible d'ouvrir le fichier.");
+}
+
+if (fwrite($create, $txt) === false) {
+    die("Impossible d'écrire dans le fichier.");
+}
+
+fclose($create);
 
 $new_folder_name = "pièces-jointes";
 $new_folder_path = $first_folder_path . "/" . $new_folder_name;
